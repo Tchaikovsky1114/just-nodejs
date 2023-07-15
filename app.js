4f715560cbbdd442fd5f34fb187b1901a3f599cb
Tchaikovsky1114 <forwarm5891@gmail.com>
2023-07-15 18:37:00 +0900

    initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+// nodejs 의 core module
+// 1. http // launch a server, send request
+// 2. https // launch a secure server(SSL)
+// 3. fs  // file system
+// 4. path // file path
+// 5. os // operating system
+const http = require('http');
+
+
+function requestListener(req, res) {
+
+}
+
+// Node.js의 아키텍쳐 : Event driven Architecture
+http.createServer((req, res) => {
+
+});
