59e22c9a9fd5b10ab0c7c3c5d2702d387ca35e1b
Tchaikovsky1114 <forwarm5891@gmail.com>
2023-07-16 17:38:27 +0900

    default path on routes

--- app.js
+++ app.js
@@ -8,7 +8,12 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.use(shopRoutes);
-app.use(adminRoutes);
+app.use('/admin',adminRoutes);
+
+// 404 page
+app.use((req, res, next) => {
+  res.status(404).send('<h1>Page not found</h1>')
+})
 
 app.listen(3000);
 
